a258fcf010638fcdf7cbd2fc062ca41bcae6a95b
snlonline <sanelenetwork@gmail.com>
2024-05-14 19:38:18 +0200

    Path and FileReader

--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace namesorter;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+    static string GetRelativeFilePath(string fileName)
+    {
+        string currentDirectory = Directory.GetCurrentDirectory();
+        string relativePath = Path.Combine("..", "..", "..", fileName);
+        return Path.GetFullPath(Path.Combine(currentDirectory, relativePath));
+    }
+    static string[] ReadFileLines(string filePath)
+    {
+        try
+        {
+            if (File.Exists(filePath))
+            {
+                return File.ReadAllLines(filePath);
+            }
+
+            Console.WriteLine($"File not found: {filePath}");
+            return Array.Empty<string>();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while reading the file: {ex.Message}");
+            return Array.Empty<string>();
+        }
+    }
+}
