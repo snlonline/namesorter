2ec144eec890aa4bbb3e0521d25aa5d415f89da4
snlonline <sanelenetwork@gmail.com>
2024-05-14 19:44:02 +0200

    being able to print/overwrite the names in the sorted list

--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        string unsortedNamesFilePath = GetRelativeFilePath("unsorted-names-list.txt");
+        string[] names = ReadFileLines(unsortedNamesFilePath);
         
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
+
+        string sortedNamesFilePath = GetRelativeFilePath("sorted-names-list.txt");
+        WriteFileLines(sortedNamesFilePath, names);
     }
     static string GetRelativeFilePath(string fileName)
     {
@@ -28,6 +37,17 @@ class Program
         {
             Console.WriteLine($"An error occurred while reading the file: {ex.Message}");
             return Array.Empty<string>();
+        }
+    }
+    static void WriteFileLines(string filePath, string[] lines)
+    {
+        try
+        {
+            File.WriteAllLines(filePath, lines);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while writing to the file: {ex.Message}");
         }
     }
 }
